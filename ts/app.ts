ba19cb68642fb803a3fdf5295f1022ada40d3920
llepere <lucas.lepere@viacesi.fr>
2017-01-04 15:39:40 +0100

    LLE - Test some changes

--- ts/app.ts
+++ ts/app.ts
@@ -18,7 +18,6 @@ class SimpleGame
 	{
 		let logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
 		logo.anchor.setTo(0.5,0.5);
-		logo.scale.setTo(0.1,0.1)
 	}
 
 	public update()
